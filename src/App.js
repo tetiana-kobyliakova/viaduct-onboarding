20a1846fe6118f668098a0caffb42f1b6c43a9cc
Tetiana <tetiana.kobyliakova@viaduct.pro>
2021-07-22 18:19:10 +0300

    Add alt attribute to img in Zoom-widget

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
 import Container from "./components/Container";
-import Widget from "./components/photo-widget";
-import ColorArray from "./components/color-array";
-import ZoomWidget from "./components/zoom-widget";
+// import Widget from "./components/photo-widget";
+// import ColorArray from "./components/color-array";
+// import ZoomWidget from "./components/zoom-widget";
 import UsersList from "./components/users-list";
 
 function App() {

4c0935d3da474d64febe412db0bd4b0445c6fa7f
Tetiana <tetiana.kobyliakova@viaduct.pro>
2021-07-27 11:10:23 +0300

    Add CallFormState component

--- src/components/form-validation2/Form2.js
+++ src/components/form-validation2/Form2.js
@@ -1,10 +1,20 @@
 import React from "react";
 import styles from "./Form2.module.css";
 import Error from "./Error";
-
 const formData = [
   {
     name: "name",
+    type: "text",
+    validationRules: [(value) => !value, (value) => value.length < 3],
+    value: "",
+    errors: [
+      "Field shouldn't be empty",
+      "Length of name should be greater than 2 characters",
+    ],
+    errorIndex: -1,
+  },
+  {
+    name: "text",
     type: "text",
     validationRules: [(value) => !value, (value) => value.length < 3],
     value: "",
@@ -31,9 +41,24 @@ const formData = [
     errorIndex: -1,
   },
 ];
-
-const Form2 = ({ state = formData }) => {
+const CallFormState = ({ state = formData }) => {
   const [formState, setFormState] = React.useState(state);
+  const successCBSubmitHandler = () => {
+    console.log("success", {
+      name: formState[0].value,
+      amount: formState[1].value,
+    });
+    setFormState(state);
+  };
+  return (
+    <Form2
+      formState={formState}
+      setFormState={setFormState}
+      successCB={successCBSubmitHandler}
+    />
+  );
+};
+const Form2 = ({ formState, setFormState, successCB }) => {
   const onInputChange = (e) => {
     setFormState(
       formState.map((item) => {
@@ -45,34 +70,26 @@ const Form2 = ({ state = formData }) => {
       })
     );
   };
-
   const validateForm = () => {
     const newState = formState.map((item, index) => {
       const errorIdx = formState[index].validationRules.findIndex((i) =>
         i(formState[index].value)
       );
-
-      return { ...item, errorIndex: errorIdx, value: "" };
+      return { ...item, errorIndex: errorIdx };
     });
-    setFormState(newState);
     return newState;
   };
   const onSubmitForm = (e) => {
     e.preventDefault();
-    const isValid = validateForm()
-      .map((item) => item.errorIndex)
-      .every((item) => item === -1);
+    const checkFormState = validateForm();
+    const isValid = checkFormState.every((item) => item.errorIndex === -1);
     if (isValid) {
-      console.log("Form was submitted");
-      console.log({ name: formState[0].value, amount: formState[1].value });
-
-      setFormState(state);
+      successCB(formState);
     } else {
       console.log("An error has occurred");
+      setFormState(checkFormState);
     }
   };
-
-  console.log(formState);
 
   return (
     <>
@@ -108,5 +125,4 @@ const Form2 = ({ state = formData }) => {
     </>
   );
 };
-
-export default Form2;
+export default CallFormState;
